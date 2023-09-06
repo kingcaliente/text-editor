5e6ca4b4d99d76216d5ec85c2d93f2c711936302
Luis Pardo <lpardo1989@gmail.com>
2023-09-05 21:00:06 -0700

    Update install.js

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -32,3 +32,33 @@ window.addEventListener('appinstalled', (event) => {
     console.log('install hit')
     window.deferredPrompt = null;
 }); 
+
+
+let deferredPrompt; 
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent the mini-infobar from appearing on mobile
+  e.preventDefault();
+  // Stash the event so it can be triggered later
+  deferredPrompt = e;
+  // Update UI notify the user they can install the PWA
+  butInstall.style.display = 'block';
+});
+
+butInstall.addEventListener('click', async () => {
+  // Hide the install prompt
+  butInstall.style.display = 'none';
+  // Show the install prompt
+  deferredPrompt.prompt();
+  // Wait for the user to respond to the prompt
+  const { outcome } = await deferredPrompt.userChoice;
+  // Optionally, send analytics event with the response
+  console.log(`User response to the install prompt: ${outcome}`);
+  // We've used the prompt, and can't use it again, throw it away
+  deferredPrompt = null;
+});
+
+window.addEventListener('appinstalled', () => {
+  // Log app installed to console
+  console.log('PWA was installed');
+});
